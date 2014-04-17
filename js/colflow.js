3ebfef81bc824e21d18531c0fca1b147e38697c7
Hakan Bilgin <hbi99@hotmail.com>
2014-04-17 14:10:34 +0200

    visual commit

---
+++ js/colflow.js
@@ -0,0 +1,19 @@
+
+// DOM Controled by Hakan Bilgin (c) 2014
+
+(function(root, document, $) {
+	'use strict';
+
+	var colflow = {
+		init: function() {
+
+		}
+	};
+
+	root.colflow = colflow;
+
+	$(document).ready(function() {
+		colflow.init();
+	});
+
+})(window, document, jQuery);
