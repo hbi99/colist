88d3da81ba4cc86200de186f84ae5e4e6f191cb0
Hakan Bilgin <hbi99@hotmail.com>
2014-04-17 15:18:05 +0200

    visual stuff

--- views/index.php
+++ views/index.php
@@ -17,6 +17,19 @@ wp_enqueue_script( array( "colflow-admin_script" ) );
 		<div class="column">
 			<div class="frame">
 				<div class="content">
+					<div class="row"><figure class="icon-folder"></figure><span>Folder 2</span></div>
+					<div class="row"><figure class="icon-folder"></figure><span>Folder 2</span></div>
+					<div class="row"><figure class="icon-folder"></figure><span>Folder 2</span></div>
+					<div class="row active"><figure class="icon-folder"></figure><span>Folder 1</span></div>
+					<div class="row"><figure class="icon-folder"></figure><span>Folder 2</span></div>
+					<div class="row"><figure class="icon-folder"></figure><span>Folder 2</span></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="column">
+			<div class="frame">
+				<div class="content">
 					<div class="row active"><figure class="icon-folder"></figure><span>Folder 1</span></div>
 					<div class="row"><figure class="icon-folder"></figure><span>Folder 2</span></div>
 					<div class="row"><figure class="icon-file-o"></figure><span>.htaccess</span></div>
@@ -41,7 +54,33 @@ wp_enqueue_script( array( "colflow-admin_script" ) );
 				<div class="content">
 					
 					<div class="file-preview">
-						<figure class="icon-picture-o"></figure>
+						<!-- filetype: text content -->
+						<pre class="file-text hideMe">
+&lt;?php
+/** PHP file
+ */
+
+	function __construct() {
+		//******** settings
+		$this->settings = array(
+			'path' => plugin_dir_url( __FILE__ ),
+			'dir' => plugin_dir_path( __FILE__ ),
+			'version' => '0.0.1'
+		);
+		//******** ajax
+		add_action( 'wp_ajax_mce_fonts/save_fonts', array( $this, 'save_fonts' ) );
+		
+		add_action( 'init', array( $this, 'init' ), 1 );
+	}
+
+?&gt;
+						</pre>
+						<!-- filetype: image content -->
+						<div class="file-image hideMe" style="background-image: url('http://frojd.theforumist/wp-content/uploads/2014/02/The_Forum_mensfashion_01-500x330.jpg');"></div>
+						<!-- default -->
+						<div class="file-generic hideMe">
+							<span class="file-extension">pdf</span>
+						</div>
 					</div>
 					<div class="file-details">
 						<div class="detail-row">Name <span>Image 2.png</span></div>
